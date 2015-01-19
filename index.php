e583efbba9052d918304378fdc3dc675ca3a9765
HumanBooster <bclerc@humanbooster.com>
2015-01-19 14:03:19 +0100

    Ajout d'un objet PDO et liste des articles

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
+
+	/* connexion BDD avec PDO */
+	$host = "localhost";
+	$bd_name = "blogphp";
+	$bd_user = "blogphp";
+	$bd_pwd = "blogpwd";
+	$dsn = "mysql:host=".$host.";dbname=".$bd_name;
+
+	$pdo = new PDO($dsn, $bd_user, $bd_pwd);
+
+
+
 	// on récupère le prenom et nom en GET
 	$prenom = isset($_GET['prenom']) ? $_GET['prenom'] : "";
 	$nom = isset($_GET['nom']) ? $_GET['nom'] : "";
@@ -21,10 +33,12 @@
 		<p> Bonjour <?php echo $prenom . " " . $nom; ?> !
 		</p>
 
+		<h2>Ajouter un article</h2>
+
 		<form name="article" method="POST" action="index.php">
 			Formulaire de soumission d'article
 			<p>Titre <input type="text" name="titre" value="<?php echo $titre; ?>" /></p>
-			<p>Contenu <input type="text" name="contenu" value="<?php echo $contenu; ?>" /></p>
+			<p>Contenu <textarea name="contenu"><?php echo $contenu; ?></textarea></p>
 			<input type="submit" name="soumis" />
 		</form>
 
@@ -32,12 +46,31 @@
 
 		<p>Vous avez saisi :<br/>
 		<b>Titre :</b> <?php echo $titre; ?> <br/>
-		<b>Contenu :</b> <?php echo $contenu; ?></p>
+		<b>Contenu :</b> <?php echo nl2br($contenu); ?></p>
 		
 		<?php } else { ?>
 
 		<p>Vous n'avez rien saisi.</p>
 
 		<?php } ?>
+
+		<h2>Liste des articles</h2>
+
+		<?php
+			$query = $pdo->query("SELECT * FROM article");
+
+			// fetch() retourne les résultats un par un
+			while ( $article = $query->fetch() ) {
+				echo '<p>'.$article['titre'].'<br/>'.$article['contenu'].'</p>';
+			}
+
+			// fetchAll retourne tous les résultats d'un coup
+			$articles = $query->fetchAll();
+
+			foreach ($articles as $article) {
+				echo '<p>'.$article['titre'].'<br/>'.$article['contenu'].'</p>';
+			}
+
+		?>
 	</body>
 </html>
